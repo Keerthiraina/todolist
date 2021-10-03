662a2a6823473d3f7b3c646ed4e7449849d010a8
keerthiraina <keerthiraina18@gmail.com>
2021-10-03 17:49:49 +0530

    final todolist

--- app.js
+++ app.js
@@ -2,26 +2,37 @@
 
 const express =require("express");
 const bodyParser =require("body-parser");
-
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+let items=[];
+
 app.set("view engine","ejs");
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  var today = new date();
+  var today = new Date();
   var options ={
     weekday: "long",
     day:"numeric",
     month: "long"
   };
-  var day = today.toLocalDateString("en-US",options);
-   res.render("list",{
-     kindiofDay: day
+  let day = today.toLocaleDateString("en-US",options);
+   res.render("index",{
+     kindiofDay: day,
+     newlistitem:items
    });
 
 
 });
+app.post("/",function(req,res){
+
+  let item = req.body.newItem;
+   items.push(item);
+  console.log(item);
+  res.redirect("/");
 
 
-app.listen(3501,function(){
+});
+app.listen(3000,function(){
   console.log("Server started on port 3000");
 });
